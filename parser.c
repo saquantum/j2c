93ce5fbef848f825bb0c92ff611e780b57a4839d
saquantum <kj24716@bris.ac.uk>
2024-12-19 23:40:20 +0000

    111

--- parser.c
+++ parser.c
@@ -1,6 +1,7 @@
 #include "parser.h"
 
 // public static final int x = 1 + 2 , y = 10 , z ;
+// ArrayList<String> list = new ArrayList<>();
 void parseLetStatement(tokenNode* n0, tokenNode* nf){
     
 }
--- parser.h
+++ parser.h
@@ -1,4 +1,10 @@
 #pragma once
 #include "lexer.h"
 
+typedef struct CSTNode{
+    
+}CSTNode;
 
+typedef struct CST{
+    
+}CST;
